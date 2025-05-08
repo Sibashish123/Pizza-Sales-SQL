f0063eea8f6b50392b8ebc7bc14389d4061cef2c
Sibashish123 <sibashishdas81@gmail.com>
2025-05-08 00:19:45 -0500

    Add files via upload

---
+++ qs7.sql
@@ -0,0 +1,4 @@
+-- Determine the distribution of orders by hour of the day.
+
+SELECT hour(order_time)AS hour, COUNT(order_id) AS order_count FROM orders
+GROUP BY hour(order_time);
---
+++ qs8.sql
@@ -0,0 +1,4 @@
+-- Join relevant tables to find the category-wise distribution of pizzas.
+
+SELECT category, count(name) from pizza_types
+GROUP BY category;
